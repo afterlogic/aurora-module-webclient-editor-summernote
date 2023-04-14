e8fdf06b473353e31673db35a7822040dc812580
nadine-shi <nadinshi@gmail.com>
2023-04-14 17:02:00 +0300

    Init

---
+++ js/manager.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = function (appData) {
+  const App = require('%PathToCoreWebclientModule%/js/App.js'),
+    ModulesManager = require('%PathToCoreWebclientModule%/js/ModulesManager.js')
+  if (!ModulesManager.isModuleAvailable('MailWebclient')) {
+    return null
+  }
+
+  if (App.isUserNormalOrTenant()) {
+    return {
+      start: function (ModulesManager) {
+        App.subscribeEvent('MailWebclient::GetCHtmlEditorView', (params) => {
+          console.log({ params })
+          params.CHtmlEditorView = require('modules/%ModuleName%/js/views/CSummernoteEditorView.js')
+        })
+      },
+    }
+  }
+
+  return null
+}
